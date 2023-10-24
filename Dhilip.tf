1b41fe1b0d9e2507fee123b3bdcdb339bc38994b
dhilipjoan <145645200+dhilip26@users.noreply.github.com>
2023-10-24 17:25:29 +0530

    Dhilip.tf

--- Dhilip.tf
+++ Dhilip.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "Subnet1" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.0.1.0/24"
-
+  map_public_ip_on_launch = true
   tags = {
     Name = "tf_SN-1"
   }
@@ -52,3 +52,89 @@ resource "aws_internet_gateway" "gw" {
     Name = "demo"
   }
 }
+
+#Subnet association 
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.Subnet1.id
+  route_table_id = aws_route_table.DR.id
+}
+
+#Security group
+resource "aws_security_group" "allow_tls" {
+  name        = "Terra"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"] 
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+ingress {
+    description      = "TLS from VPC"
+    from_port        = 2049
+    to_port          = 2049
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+ingress {
+    description      = "TLS from VPC"
+    from_port        = -1
+    to_port          = -1
+    protocol         = "ICMP"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "TESTSG"
+  }
+}
+
+#Key_Pair
+# Generates a secure private key and encodes it as PEM
+resource "tls_private_key" "key_pair" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create the Key Pair
+resource "aws_key_pair" "Testkey" {
+  key_name   = "Terrakey"  
+  public_key = tls_private_key.key_pair.public_key_openssh
+}
+
+# Save file
+resource "local_file" "ssh_key" {
+  filename = "Terrakey.pem"
+  content  = tls_private_key.key_pair.private_key_pem
+}
+
